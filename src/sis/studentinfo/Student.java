91ee91f9313a3c897f1836b021d15fb7b670111d
emaphis <emaphis85@gmail.com>
2017-02-09 16:20:53 -0500

    Add custom values to Grade enum.

--- src/sis/studentinfo/Student.java
+++ src/sis/studentinfo/Student.java
@@ -2,13 +2,32 @@ package sis.studentinfo;
 
 import java.util.*;
 
+
 public class Student {
 	private String name;
 	private int credits;
 	static final int CREDITS_REQUIRED_FOR_FULL_TIME = 12;
 	static final String IN_STATE = "CO";
 	private String state = "";
-	public enum Grade {A, B, C, D, F };
+
+	public enum Grade {
+		A(4),
+		B(3),
+		C(2),
+		D(1),
+		F(0);
+
+		private int points;
+
+		private Grade (int points) {
+			this.points = points;
+		}
+
+		int getPoints() {
+			return points;
+		}
+	}
+
 	private List<Grade> grades = new ArrayList<>();
 	private GradingStratagy gradingStrategy = new BasicGradingStrategy();
 
